6a99044dfae7ce5459849627f49c15b6f9cda871
EA - Gustavo Xavier Teixeira De Souza <gustavo_xavier@educadventista.org>
2023-09-11 19:35:32 -0300

    aula 11-09

---
+++ 3_TEMPLATE_ENGINE/5_repeticao/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+//configurar o hasndlebars como template engine
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/dashboard', (req, res) => {
+    const itens = ['item 1', 'item 2', 'item 3']
+
+    res.render('dashboard', { itens })
+})
+
+app.get('/', (req, res) => {
+    const user = {
+        name: "Gustavo Xavier",
+        age: 21
+    }
+
+    const auth = true
+    res.render('home', {user, auth})
+})
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000')
+})
